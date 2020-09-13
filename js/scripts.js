b1eddcacfd8c2217bbc9eb7a5d7fe1dfd72921b1
Guishe <gui.bar.jim@gmail.com>
2020-09-13 15:43:30 -0300

    viendolo asi, faltarian los hovers y cambios esteticos.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,10 @@ window.onscroll = function(){
     let navbar = document.getElementById('navbar')
 
     if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-        navbar.classList.remove('bg-transparent');
         navbar.classList.add('bg-dark');
         ;
       } else {
-        navbar.classList.remove('bg-dark');          
-        navbar.classList.add('bg-transparent');
+        navbar.classList.remove('bg-dark');
       }
 }
 
