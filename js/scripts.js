720b3dd3a0616701b9e9688860b2d25c8962cf16
Guishe <gui.bar.jim@gmail.com>
2020-09-13 13:21:33 -0300

    Fuente agregada, se avanzo en los componentes tambien

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
-let body = document.getElementsByTagName('body');
+// NAVBAR TRANSPARENTE ON TOP=0
+window.onscroll = function(){
 
+    let navbar = document.getElementById('navbar')
+
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+        navbar.classList.remove('bg-transparent');
+        navbar.classList.add('bg-dark');
+        ;
+      } else {
+        navbar.classList.remove('bg-dark');          
+        navbar.classList.add('bg-transparent');
+      }
+}
+
+// ROTATE CHEVRON
